b415ebc89961f65b5c247e0b8194ec109408a351
Woo Young Kim <kimwy012@gmail.com>
2022-02-18 22:01:56 -0800

    initial directory

--- src/App.js
+++ src/App.js
@@ -4,20 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className="logo" />
+        <span className="title">IATRI</span>
       </header>
+      <div>
+        <div className='searchbar'></div>
+      </div>
     </div>
   );
 }
